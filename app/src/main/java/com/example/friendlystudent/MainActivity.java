bf81a92bf05158c32685d395ead808b9f4fdedec
Idan Hazon <idanhazon1@gmail.com>
2022-11-21 16:46:08 +0200

    Update_1

--- app/src/main/java/com/example/friendlystudent/MainActivity.java
+++ app/src/main/java/com/example/friendlystudent/MainActivity.java
@@ -7,12 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth= FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(mAuth.getCurrentUser()!=null){
+            //התחבר
+        }
 
     }
 
